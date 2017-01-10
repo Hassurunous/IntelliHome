6c6ea220173f0dd0134cb4901a152d293ca28c73
Michael Loubier <michael.a.loubier@gmail.com>
2017-01-09 18:05:29 -0800

    Working on adding functionality to store selected features in 'My List', shopping-cart style.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,51 @@
 class SessionsController < ApplicationController
-  def new
+
+  #Initialize User List Session
+  def initialize(session)
+      @session = session
+      @session[:list] ||= {}
   end
+
+  def show
+    @features = session[:list][:features]
+  end
+
+  def add_to_list(feature)
+    session[:list] << feature
+  end
+
+  #List Count
+  def list_count
+      if (@session[:list][:features] && @session[:list][:features] != {})
+          @session[:list][:features].count
+      else
+          0
+      end
+  end
+
+  #List Contents
+  def list_contents
+        features = @session[:list][:features]
+
+        if (features && features != {})
+
+            #Determine Quantities
+            quantities = Hash[features.uniq.map {|i| [i, features.count(i)]}]
+
+            #Get features from DB
+            features_array = feature.find(features.uniq)
+
+            #Create Qty Array
+            features_new = {}
+            features_array.each{
+                |a| features_new[a] = {"qty" => quantities[a.id.to_s]}
+            }
+
+            #Output appended
+            return features_new
+
+        end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show]
   resources :features, only: [:index, :show]
   resources :products, only: [:index]
+  resources :sessions, only: [:show]
 
-  # root to: "features#index"
   root to: "categories#index"
+
+  #Route to direct Feature links to add Feature to "My List"
+  post "session/add_to_list", to: "sessions#add_to_list", as: "add_to_list"
 
 end
