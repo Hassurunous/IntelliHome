6a40324c308ae447e04e4b8844b6ac7f5ae7bf6c
Michael Loubier <michael.a.loubier@gmail.com>
2017-01-16 19:28:49 -0800

    Fixed logout.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,8 +13,16 @@ module SessionsHelper
 
     # Logs out the current user
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    # Forgets a persistent session.
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
     end
 
     # Returns the current logged-in user (if any).
